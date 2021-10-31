346cbfd3a32f2e6eb0430a08882222b076673a5e
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-10-31 20:09:11 +0300

    Add OkHttp and make simple call to API with callback

---
+++ app/src/main/java/com/itmo/moonphase/Consts.kt
@@ -0,0 +1,6 @@
+package com.itmo.moonphase
+
+object Consts {
+    const val BASE_URL = "http://api.farmsense.net/v1"
+    const val MOON_PHASE_URL = "$BASE_URL/moonphases"
+}
--- app/src/main/java/com/itmo/moonphase/MainActivity.kt
+++ app/src/main/java/com/itmo/moonphase/MainActivity.kt
@@ -2,10 +2,34 @@ package com.itmo.moonphase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    // OkHttp Recipes https://square.github.io/okhttp/recipes/#asynchronous-get-kt-java
+    // Using OkHttp https://guides.codepath.com/android/Using-OkHttp
+    // Kotlin on Android development: Image download & display using OkHttp https://www.youtube.com/watch?v=HzPmbzIVxDo
+    private val httpClient = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val request = Request.Builder()
+            .url(Consts.MOON_PHASE_URL)
+            .get()
+            .build()
+
+        httpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("MOON", response.body?.string() ?: "")
+            }
+        })
     }
 }
