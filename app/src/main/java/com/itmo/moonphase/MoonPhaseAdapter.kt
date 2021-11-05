0d56859ed4658e3bf6aa78fa96080e424b2c3439
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-05 16:21:05 +0300

    MPA/MPVH. Create class MoonPhaseResource and add getMoonPhaseResource() insead of getName() and getImage()

--- app/src/main/java/com/itmo/moonphase/MoonPhaseAdapter.kt
+++ app/src/main/java/com/itmo/moonphase/MoonPhaseAdapter.kt
@@ -17,44 +17,6 @@ class MoonPhaseAdapter(
     private val dateTimeFormatter = DateTimeFormatter.ofPattern(context.getString(R.string.moonPhase_dateTime_format))
     private val percentFormatter = NumberFormat.getPercentInstance().apply { minimumFractionDigits = 0 }
 
-    class MoonPhaseViewHolder(
-        private val binding: ListItemMoonPhaseBinding,
-        private val dateTimeFormatter: DateTimeFormatter,
-        private val percentFormatter: NumberFormat,
-    ) : RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(moonPhaseInfo: MoonPhaseInfo) = binding.let { with(moonPhaseInfo) {
-            it.tvMoonPhaseDate.text = dateTimeFormatter.format(dateTime)
-            it.imgMoonPhase.setImageResource(getMoonPhaseImage(phase))
-            it.tvMoonPhaseName.text = getMoonPhaseName(phase)
-            it.tvMoonPhaseAge.text = age.roundToInt().toString()
-            it.tvMoonPhaseIllumination.text = percentFormatter.format(illumination)
-        } }
-
-        private fun getMoonPhaseImage(moonPhase: MoonPhaseEnum) = when(moonPhase) {
-            MoonPhaseEnum.NEW_MOON -> R.drawable.emoji_new_moon
-            MoonPhaseEnum.WAXING_CRESCENT -> R.drawable.emoji_waxing_crescent
-            MoonPhaseEnum.FIRST_QUARTER -> R.drawable.emoji_first_quarter
-            MoonPhaseEnum.WAXING_GIBBOUS -> R.drawable.emoji_waxing_gibbous
-            MoonPhaseEnum.FULL_MOON -> R.drawable.emoji_full_moon
-            MoonPhaseEnum.WANING_GIBBOUS -> R.drawable.emoji_wanning_gibbous
-            MoonPhaseEnum.LAST_QUARTER -> R.drawable.emoji_last_quarter
-            MoonPhaseEnum.WANING_CRESCENT -> R.drawable.emoji_wanning_crescent
-        }
-
-        private fun getMoonPhaseName(moonPhase: MoonPhaseEnum) = with(binding.root.context) { when(moonPhase) {
-            MoonPhaseEnum.NEW_MOON -> getString(R.string.moonPhase_newMoon)
-            MoonPhaseEnum.WAXING_CRESCENT -> getString(R.string.moonPhase_waxingCrescent)
-            MoonPhaseEnum.FIRST_QUARTER -> getString(R.string.moonPhase_firstQuarter)
-            MoonPhaseEnum.WAXING_GIBBOUS -> getString(R.string.moonPhase_waxingGibbous)
-            MoonPhaseEnum.FULL_MOON -> getString(R.string.moonPhase_fullMoon)
-            MoonPhaseEnum.WANING_GIBBOUS -> getString(R.string.moonPhase_waningGibbous)
-            MoonPhaseEnum.LAST_QUARTER -> getString(R.string.moonPhase_lastQuarter)
-            MoonPhaseEnum.WANING_CRESCENT -> getString(R.string.moonPhase_waningCrescent)
-        } }
-
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoonPhaseViewHolder {
         val binding = ListItemMoonPhaseBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -68,4 +30,36 @@ class MoonPhaseAdapter(
         holder.bind(moonPhases[position])
 
     override fun getItemCount() = moonPhases.size
+
+    class MoonPhaseViewHolder(
+        private val binding: ListItemMoonPhaseBinding,
+        private val dateTimeFormatter: DateTimeFormatter,
+        private val percentFormatter: NumberFormat,
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(moonPhaseInfo: MoonPhaseInfo) = binding.let { with(moonPhaseInfo) {
+            getMoonPhaseResource(phase).let { mpr ->
+                it.imgMoonPhase.setImageResource(mpr.imageId)
+                it.tvMoonPhaseName.text = mpr.name
+            }
+
+            it.tvMoonPhaseDate.text = dateTimeFormatter.format(dateTime)
+            it.tvMoonPhaseAge.text = age.roundToInt().toString()
+            it.tvMoonPhaseIllumination.text = percentFormatter.format(illumination)
+        } }
+
+        private fun getMoonPhaseResource(moonPhase: MoonPhaseEnum) = with(binding.root.context) { when(moonPhase) {
+            MoonPhaseEnum.NEW_MOON -> MoonPhaseResource(R.drawable.emoji_new_moon, getString(R.string.moonPhase_newMoon))
+            MoonPhaseEnum.WAXING_CRESCENT -> MoonPhaseResource(R.drawable.emoji_waxing_crescent, getString(R.string.moonPhase_waxingCrescent))
+            MoonPhaseEnum.FIRST_QUARTER -> MoonPhaseResource(R.drawable.emoji_first_quarter, getString(R.string.moonPhase_firstQuarter))
+            MoonPhaseEnum.WAXING_GIBBOUS -> MoonPhaseResource(R.drawable.emoji_waxing_gibbous, getString(R.string.moonPhase_waxingGibbous))
+            MoonPhaseEnum.FULL_MOON -> MoonPhaseResource(R.drawable.emoji_full_moon, getString(R.string.moonPhase_fullMoon))
+            MoonPhaseEnum.WANING_GIBBOUS -> MoonPhaseResource(R.drawable.emoji_wanning_gibbous, getString(R.string.moonPhase_waningGibbous))
+            MoonPhaseEnum.LAST_QUARTER -> MoonPhaseResource(R.drawable.emoji_last_quarter, getString(R.string.moonPhase_lastQuarter))
+            MoonPhaseEnum.WANING_CRESCENT -> MoonPhaseResource(R.drawable.emoji_wanning_crescent, getString(R.string.moonPhase_waningCrescent))
+        } }
+
+        data class MoonPhaseResource(val imageId: Int, val name: String)
+
+    }
 }
