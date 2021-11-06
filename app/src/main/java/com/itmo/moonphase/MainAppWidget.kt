994e30f08ae264df6ff22240a6386031ee94c3b0
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-06 14:53:44 +0300

    Widget. Add getting moonPhase from API and updating view

--- app/src/main/java/com/itmo/moonphase/MainAppWidget.kt
+++ app/src/main/java/com/itmo/moonphase/MainAppWidget.kt
@@ -4,9 +4,17 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import com.itmo.moonphase.api.farmsense.MoonPhaseProviderFarmsense
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 // Android App Widgets Tutorial (Coffee Log) https://www.raywenderlich.com/33-android-app-widgets-tutorial
 class MainAppWidget : AppWidgetProvider() {
+
+    private val moonPhaseProvider = MoonPhaseProviderFarmsense()
 
     override fun onUpdate(
         context: Context,
@@ -14,7 +22,7 @@ class MainAppWidget : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId, moonPhaseProvider)
         }
     }
 
@@ -23,9 +31,21 @@ class MainAppWidget : AppWidgetProvider() {
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
-    appWidgetId: Int
+    appWidgetId: Int,
+    moonPhaseProvider: MoonPhaseProvider
 ) {
+    val dateTimeFormatter = DateTimeFormatter.ofPattern(context.getString(R.string.moonPhase_dateTime_format))
+
     val views = RemoteViews(context.packageName, R.layout.main_app_widget)
 
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    GlobalScope.launch {
+        val currentDateTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
+        val moonPhases = moonPhaseProvider.getMoonPhases(currentDateTime)
+
+        val moonPhase = moonPhases[0]
+        views.setTextViewText(R.id.wd_tv_date, dateTimeFormatter.format(moonPhase.dateTime))
+        views.setImageViewResource(R.id.wd_img_moonPhase, MoonPhaseResource.getMoonPhaseResource(moonPhase.phase).imageId)
+
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
 }
