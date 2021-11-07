4a70541bd0b164df3a4ae7fa38384f0c7e1df634
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-07 18:15:14 +0300

    SettingsActivity. Add populating spinner list with moon phases

--- app/src/main/java/com/itmo/moonphase/activities/SettingsActivity.kt
+++ app/src/main/java/com/itmo/moonphase/activities/SettingsActivity.kt
@@ -2,11 +2,32 @@ package com.itmo.moonphase.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.itmo.moonphase.MoonPhaseEnum
 import com.itmo.moonphase.R
+import com.itmo.moonphase.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initializeComponents()
     }
+
+    private fun initializeComponents() = with(binding) {
+        spnNotificationMoonPhases.adapter = ArrayAdapter(
+            baseContext,
+            R.layout.support_simple_spinner_dropdown_item,
+            MoonPhaseEnum.values().map { getString(it.nameId) }
+        ).apply {
+            setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        }
+    }
+
 }
