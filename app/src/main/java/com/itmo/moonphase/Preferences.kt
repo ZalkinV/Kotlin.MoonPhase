d247c222d14a7f8307b55fe6e3f3233ad3160468
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-07 19:51:56 +0300

    SettingsActivity. Add saving preferences of notifications and use it in initializeComponents()

---
+++ app/src/main/java/com/itmo/moonphase/Preferences.kt
@@ -0,0 +1,12 @@
+package com.itmo.moonphase
+
+object Preferences {
+
+    const val NAME = "MOON_PHASE_APP_PREFERENCES"
+
+    enum class Settings {
+        IS_NOTIFICATION_ENABLED,
+        MOON_PHASE_TO_NOTIFY,
+    }
+
+}
--- app/src/main/java/com/itmo/moonphase/activities/SettingsActivity.kt
+++ app/src/main/java/com/itmo/moonphase/activities/SettingsActivity.kt
@@ -1,15 +1,19 @@
 package com.itmo.moonphase.activities
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import com.itmo.moonphase.MoonPhaseEnum
+import com.itmo.moonphase.Preferences
 import com.itmo.moonphase.R
 import com.itmo.moonphase.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySettingsBinding
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,7 @@ class SettingsActivity : AppCompatActivity() {
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initializePreferences()
         initializeComponents()
     }
 
@@ -28,6 +33,28 @@ class SettingsActivity : AppCompatActivity() {
         ).apply {
             setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
         }
+
+        smNotification.setOnCheckedChangeListener { _, isChecked -> spnNotificationMoonPhases.isEnabled = isChecked }
+
+        smNotification.isChecked = preferences.getBoolean(Preferences.Settings.IS_NOTIFICATION_ENABLED.name, false)
+        spnNotificationMoonPhases.setSelection(
+            preferences.getInt(Preferences.Settings.MOON_PHASE_TO_NOTIFY.name, MoonPhaseEnum.NEW_MOON.ordinal))
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        with(preferences.edit()) { with (binding) {
+            putBoolean(Preferences.Settings.IS_NOTIFICATION_ENABLED.name, smNotification.isChecked)
+            putInt(Preferences.Settings.MOON_PHASE_TO_NOTIFY.name, spnNotificationMoonPhases.selectedItemPosition)
+            apply()
+        } }
+    }
+
+    // SharedPreferences http://developer.alexanderklimov.ru/android/theory/sharedpreferences.php
+    // Save key-value data https://developer.android.com/training/data-storage/shared-preferences
+    private fun initializePreferences() {
+        preferences = getSharedPreferences(Preferences.NAME, Context.MODE_PRIVATE)
     }
 
 }
