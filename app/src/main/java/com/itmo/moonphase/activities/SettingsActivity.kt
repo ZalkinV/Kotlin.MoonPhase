f280bc009215bd52c16bb5d3c2a2ce294979ebdd
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-07 17:01:55 +0300

    Create empty SettingsActivity and layout

---
+++ app/src/main/java/com/itmo/moonphase/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.itmo.moonphase.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.itmo.moonphase.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
