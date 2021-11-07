0b197dcfc267365a624c383eadc282de4214a667
Viktor Zalkin <zalkin.viktor@mail.ru>
2021-11-07 13:12:03 +0300

    MainActivity. Add initializeWorkers() with creating worker with PeriodicWorkRequest

--- app/src/main/java/com/itmo/moonphase/Consts.kt
+++ app/src/main/java/com/itmo/moonphase/Consts.kt
@@ -2,4 +2,6 @@ package com.itmo.moonphase
 
 object Consts {
     const val FORECAST_DURATION_DAYS = 30L
+
+    const val WORKER_PERIOD_HOURS = 2L
 }
--- app/src/main/java/com/itmo/moonphase/MainActivity.kt
+++ app/src/main/java/com/itmo/moonphase/MainActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.itmo.moonphase.api.farmsense.MoonPhaseProviderFarmsense
 import com.itmo.moonphase.databinding.ActivityMainBinding
+import com.itmo.moonphase.workers.MoonPhaseWorker
 import kotlinx.coroutines.launch
 import java.lang.RuntimeException
+import java.util.concurrent.TimeUnit
 
 // Android Kotlin Coroutine Scope for Activity, Fragment and ViewModel (Architecture Components): https://code.luasoftware.com/tutorials/android/android-kotlin-coroutine-scope-for-activity/
 class MainActivity : AppCompatActivity() {
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initializeComponents()
+        initializeWorkers()
 
         lifecycleScope.launch {
             try {
@@ -53,6 +58,15 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(baseContext)
             setHasFixedSize(true)
         }
+    }
+
+    // Define work requests: Schedule periodic work https://developer.android.com/topic/libraries/architecture/workmanager/how-to/define-work#schedule_periodic_work
+    private fun initializeWorkers() {
+        val request = PeriodicWorkRequestBuilder<MoonPhaseWorker>(Consts.WORKER_PERIOD_HOURS, TimeUnit.HOURS)
+            .build()
+
+        WorkManager.getInstance(baseContext)
+            .enqueue(request)
     }
 
 }
